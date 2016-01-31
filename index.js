d2a2234730ee2bd7a9031c9ce862d5dfcdd58528
HTF <457296373@qq.com>
2016-01-31 14:52:14 +0800

    update

--- index.js
+++ index.js
@@ -3,22 +3,45 @@
 const request = require('request');
 const cheerio = require('cheerio');
 
-const url = 'https://www.zhihu.com/question/39943474';
+let url = 'https://www.zhihu.com/topic/19569535';
 
-request(url, (err, resp, body) => {
-	if(!err && resp.statusCode == 200){
-		process(body);
-	}
-})
+httpRequest(url, process);
+
+// http 请求
+function httpRequest(url, callback) {
+	request(url, (err, resp, body) => {
+		if(!err && resp.statusCode == 200){
+			callback(body, url);
+		}
+	})
+}
 
 // 主处理函数
-function process(data) {
+function process(data){
 
-	const $ = cheerio.load(data);
-	const answerNum = $('#zh-question-answer-num').attr('data-num') || 0;
-	const followNum = $('#zh-question-side-header-wrap').text().match(/\d+/ig);
+	let $ = cheerio.load(data);
+	let questions = $('.feed-item .content h2 a').toArray();
+	questions.forEach((ques) => {
+		let url = 'https://www.zhihu.com' + ques.attribs.href;
+		httpRequest(url, parseEach);
+	})
+}
 
-	console.log('回答人数: ' + answerNum);
-	console.log('关注人数: ' + followNum);
+// 解析每个问题
+function parseEach(data, url) {
+
+	let $ = cheerio.load(data);
+	let title = $('#zh-question-title .zm-item-title').text().replace(/(^\s+)|(\s+$)/g,"");
+	let answer = $('#zh-question-answer-num').attr('data-num') || 0;
+	let follow = $('#zh-question-side-header-wrap').text().match(/\d+/ig)[0];
+
+	let ques = {
+		title: title,
+		url: url,
+		answer: Number(answer),
+		follow: Number(follow)
+	}
+
+	console.log(ques);
 
 }
